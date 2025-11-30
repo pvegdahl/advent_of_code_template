d3bab7193f1daced991da616a5107a1928779647
Philip Vegdahl <pvegdahl@gmail.com>
2025-11-30 10:24:29 -0500

    Bump Elixir 1.19

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCodeTemplate.MixProject do
     [
       app: :advent_of_code_template,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
